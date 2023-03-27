adaa0b3777e2cb4559b2b2e76170f5791d3732dd
lelis <lelis685@gmail.com>
2023-03-27 07:23:57 -0300

    chaining consumer

--- src/main/java/br/com/demo/MainConsumer.java
+++ src/main/java/br/com/demo/MainConsumer.java
@@ -1,9 +1,21 @@
 package br.com.demo;
 
+
+import br.com.demo.function.Consumer;
+
+
 public class MainConsumer {
 
 
     public static void main(String[] args) {
+
+
+        Consumer<String> c1 = s -> System.out.println("c1= " + s);
+        Consumer<String> c2 = s -> System.out.println("c2= " + s);
+
+        Consumer<String> c3 = c1.andThen(c2);
+        c3.accept("hello");
+
 
     }
 
---
+++ src/main/java/br/com/demo/function/Consumer.java
@@ -0,0 +1,14 @@
+package br.com.demo.function;
+
+@FunctionalInterface
+public interface Consumer<T> {
+
+    void accept(T t);
+
+    default Consumer<T> andThen(Consumer<T> other) {
+        return (T t) -> {
+            this.accept(t);
+            other.accept(t);
+        };
+    }
+}
